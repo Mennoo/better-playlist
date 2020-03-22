ee477c645bd4b82d198647a26a9e1a163d2bdbe7
Menno <mennoo@gmail.com>
2020-03-22 16:49:42 +0100

    update met wat css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
+import Logo from './logo.svg';
 import './App.css';
 
 let name = "Menno"
 
+let serverData = {}; // aanmaken data iets..
+let fakeServerData ={
+  user: {
+    name: 'Menno'
+  }
+};
+
 let defaultStyle = {
-  color: '#666'
+  color: '#fff'
 };
 
 class Agregate extends Component{
@@ -32,27 +39,42 @@ class Filter extends Component{
 class Playlist extends Component{
   render(){
     return(
-      <div style={{...defaultStyle, width:'25%', display:'inline-block'}}>
-        <img/>
-        <h3> Playlist Name</h3>
-        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      <div className='Playlist'>
+        <h3><img src={Logo} className='Logo' width='20px' height='20px' alt='titel'/>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
       </div>
     );
   }
 }
 
-function App() {
+class App extends Component{ // base class
+  constructor(){ //dit deel is nodig als een soort startscript
+    super(); 
+    this.state = {serverData}
+  }
+  componentDidMount(){ //dit deel is nodig als een eenmalig doorlopen indien geladen
+    this.setState({serverData: fakeServerData});
+  }
+  render(){
   return (
     <div className="App">
-        <h1 style={defaultStyle}>Title</h1>
+        <h1 style={defaultStyle}>
+        {this.state.serverData.user && 
+        this.state.serverData.user.name}'s whicked playlist</h1>
         <Agregate/>
         <Agregate/>
         <Filter/>
         <Playlist/>
         <Playlist/>
         <Playlist/>
+        <Playlist/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
